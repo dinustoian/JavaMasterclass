533b52031cd22d5556c25e9d9e73d3adf7ff62c5
Dinu <stoian.dinu@gmail.com>
2025-12-28 12:26:51 +0200

    Added EqualSumChecker

---
+++ src/Exercises/Section5/EqualSumChecker.java
@@ -0,0 +1,19 @@
+package Exercises.Section5;
+
+public class EqualSumChecker {
+
+    public static void main(String[] args) {
+        System.out.println(
+                hasEqualSum(1, 1, 1) + " " +
+                hasEqualSum(1, 1, 2) + " " +
+                hasEqualSum(1, -1, 0)
+        );
+    }
+
+    public static boolean hasEqualSum (int firstNumber, int secondNumber, int thirdNumber){
+
+        return (firstNumber + secondNumber) == thirdNumber;
+
+    }
+
+}
