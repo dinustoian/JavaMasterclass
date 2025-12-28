f57e5c08dfb2cc05f9e3124203d0f8e0d322eeaa
Dinu <stoian.dinu@gmail.com>
2025-12-28 14:50:14 +0200

    Done all from Section 5

---
+++ src/Exercises/Section5/PlayingCat.java
@@ -0,0 +1,21 @@
+package Exercises.Section5;
+
+public class PlayingCat {
+
+    public static void main(String[] args) {
+        System.out.println(
+                isCatPlaying(true, 10) + " " +
+                isCatPlaying(false, 36) + " " +
+                isCatPlaying(false, 35)
+        );
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature){
+        if (summer){
+            return temperature >= 25 && temperature <= 45;
+        } else {
+            return temperature >= 25 && temperature <= 35;
+        }
+    }
+
+}
