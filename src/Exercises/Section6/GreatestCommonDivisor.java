eae4a5b64d68019792b00fe75991c1e38a13373c
Dinu <stoian.dinu@gmail.com>
2026-01-01 15:34:28 +0200

    Added more exercises

---
+++ src/Exercises/Section6/GreatestCommonDivisor.java
@@ -0,0 +1,28 @@
+package Exercises.Section6;
+
+public class GreatestCommonDivisor {
+
+    public static void main(String[] args) {
+        System.out.println(getGreatestCommonDivisor(25, 15));
+        System.out.println(getGreatestCommonDivisor(12, 30));
+        System.out.println(getGreatestCommonDivisor(9, 18));
+        System.out.println(getGreatestCommonDivisor(81, 153));
+    }
+
+    public static int getGreatestCommonDivisor (int first, int second) {
+
+        if (first < 10 || second < 10) {
+            return -1;
+        }
+
+        int min = first < second ? first : second;
+        int gcd = 1;
+
+        for (int i=1; i<= min; i++){
+            if (first % i == 0 && second % i == 0) gcd = i;
+        }
+
+        return gcd;
+
+    }
+}
