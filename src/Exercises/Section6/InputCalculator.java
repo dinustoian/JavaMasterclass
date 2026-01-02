10ca58bf39b7a9d8752af97457f9de39507a5548
Dinu <stoian.dinu@gmail.com>
2026-01-02 12:37:41 +0200

    Added more exercises

---
+++ src/Exercises/Section6/InputCalculator.java
@@ -0,0 +1,42 @@
+package Exercises.Section6;
+
+import java.util.Scanner;
+
+public class InputCalculator {
+
+    public static void main(String[] args) {
+        inputThenPrintSumAndAverage();
+    }
+
+    public static void inputThenPrintSumAndAverage(){
+        int sum = 0;
+        int count = 0;
+        long avg = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            boolean hasNextInt = scanner.hasNextInt();
+
+            if (!hasNextInt) {
+                break;
+            }
+
+            sum += scanner.nextInt();
+            count++;
+
+            scanner.nextLine();
+        }
+
+        if (count > 0) {
+            avg = Math.round((double) sum / count);
+        }
+
+        System.out.println("SUM = " + sum + " AVG = " + avg);
+        scanner.close();
+
+    }
+
+
+}
+
