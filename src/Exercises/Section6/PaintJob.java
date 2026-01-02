3998ec757aac6f62a489853deab3626969a57ff6
Dinu <stoian.dinu@gmail.com>
2026-01-02 12:44:27 +0200

    Added more exercises

---
+++ src/Exercises/Section6/PaintJob.java
@@ -0,0 +1,45 @@
+package Exercises.Section6;
+
+public class PaintJob {
+
+    public static void main(String[] args) {
+        System.out.println(getBucketCount(-3.4, 2.1, 1.5, 2));
+        System.out.println(getBucketCount(3.4, 2.1, 1.5, 2));
+        System.out.println(getBucketCount(2.75, 3.25, 2.5, 1));
+
+        System.out.println(getBucketCount(-3.4, 2.1, 1.5));
+        System.out.println(getBucketCount(3.4, 2.1, 1.5));
+        System.out.println(getBucketCount(7.25, 4.3, 2.35));
+
+        System.out.println(getBucketCount(3.4, 1.5));
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) {
+            return -1;
+        }
+
+        double area = width * height;
+        int totalBucketsNeeded = (int) Math.ceil(area / areaPerBucket);
+        int bucketsToBuy = totalBucketsNeeded - extraBuckets;
+
+        return Math.max(bucketsToBuy, 0);
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+
+        double area = width * height;
+        return (int) Math.ceil(area / areaPerBucket);
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket) {
+        if (area <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+
+        return (int) Math.ceil(area / areaPerBucket);
+    }
+}
